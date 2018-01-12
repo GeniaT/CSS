06f00b63c896ff50c6753c728f5846b95bf50832
Genia <gtchistiakov@gmail.com>
2018-01-12 16:54:15 +1000

    initial commit

---
+++ canvas/tetris/canvas.js
@@ -0,0 +1,7 @@
+// Initial Setup
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+//Original Tetris format is 10 x 22 squares, I keep this ratio. 
+canvas.width = 200;
+canvas.height = 440;
